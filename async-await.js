db2970fa07c548d9595a3224455774f837b18a72
michaelbspur <55725624+michaelbspur@users.noreply.github.com>
2021-03-07 11:08:29 -0700

    Add files via upload

---
+++ async-await.js
@@ -0,0 +1,19 @@
+function getNumber(n) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(n), 500);
+    });
+}
+
+const getX = () => getNumber(3);
+const getY = () => getNumber(5);
+const getZ = () => getNumber(2);
+
+async function sumData() {
+    let x = await getX();
+    let y = await getY();
+    let z = await getZ();
+
+    console.log(x + y + z);
+}
+
+sumData(); 
---
+++ promises.js
@@ -0,0 +1,13 @@
+function getData() {
+    const data = [1, 2, 3];
+
+    return new Promise(resolve => {
+        setTimeout(() => {
+            const sum = data.reduce((a, b) => a + b, 0);
+            console.log(sum);
+            resolve(sum)
+        }, 1000);
+    });
+}
+
+getData().then();  
